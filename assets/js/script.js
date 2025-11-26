5f76454a98793d8d26300b8d08bf77b8d48fa2eb
Rahul <rahulnannvare07@gmail.com>
2025-11-26 12:53:56 +0530

    Fix:Actual vs Estimate

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,7 @@
  */
 ( function( $ ) {
 	$(document).ready(function() {
-		_load_accordion();
 		_load_current();
-		function _load_accordion(){
-			$('.accordion>a').on("click", function() {
-				var $this = $(this);
-				var $parent = $(this).closest('.accordion');
-				$this.toggleClass("active");
-				var panel = $('>ul',$parent);
-				if (panel.css("display") === "block") {
-					panel.css("display", "none");
-				} else {
-					panel.css("display", "block");
-				}
-			});
-		}
 		function _load_current(){
 			$("a",'#documenter_nav').on( 'click', function(e) {
 				//$accordions = $(this).parents('.accordion');
